9bb9ae31ad272658be51bc1d07a61e0da67b6b6e
Hammad Khan <59400114+ha36ad@users.noreply.github.com>
2020-06-17 19:35:53 -0400

    Update Blowfish.java

--- Blowfish.java
+++ Blowfish.java
@@ -1,8 +1,30 @@
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+import java.util.Scanner;
+
+}
 public static String blowfish (String password); {
-    try {
-    byte [] keyData = (key).getBytes();
-    SecretKeySpec secretKeySpec = new SecretKeySpec (keyData, s:"Blowfish");
+     //Ask user for a message + key
+    public static void main(String[] args) {
+    InputKey = new Scanner(System.in);
+    System.out.println("Enter your key");
+    String KeyInput = KeyInput.nextLine()
+    String key;
+    InputMessage = new Scanner(System.in);
+    System.out.println("Enter message");
+    String MessageInput = MessageInput.nextLine()
+    String message;
+        
     }
+    byte [] KeyData = (key).getBytes();
+    SecretKeySpec SK = new SecretKeySpec (MessageData, "Blowfish");
+    Cipher cipher = Cipher.getInstance ("Blowfish");
+    cipher.int(Cipher.ENCRYPT_MODE, securekey);
+    
+   
+    //Encrypting the message
+    byte [] encrypted = cipher.doFinal (message.getBytes());
+        
+        
 }
