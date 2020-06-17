049543ed4c2b84e0c55113ca6064afbdd821e75a
Hammad Khan <59400114+ha36ad@users.noreply.github.com>
2020-06-17 18:47:57 -0400

    Update Blowfish.java

--- Blowfish.java
+++ Blowfish.java
@@ -1,1 +1,8 @@
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+public static String blowfish (String password); {
+    try {
+    byte [] keyData = (key).getBytes();
 
+    }
+}
