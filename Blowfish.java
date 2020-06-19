ea4be02ab4ef09ca613d2645acd3ef6b05aadf46
Hammad Khan <59400114+ha36ad@users.noreply.github.com>
2020-06-19 19:08:47 -0400

    Update Blowfish.java

--- Blowfish.java
+++ Blowfish.java
@@ -11,9 +11,9 @@ public class Blowfish {
        //Asks user for a message
         Scanner InputMessage = new Scanner(System.in);
         System.out.println("Enter message");
-        String Message = InputMessage.nextLine();  // Read user input
+        String Message = InputMessage.nextLine();  
         System.out.println("Your message in plaintext is: " + Message);
-
+       
  //Key generation
         KeyGenerator kgen = KeyGenerator.getInstance("Blowfish");
         SecretKey skey = kgen.generateKey();
@@ -24,11 +24,14 @@ public class Blowfish {
         Cipher cipher = Cipher.getInstance("Blowfish");
         cipher.init(Cipher.ENCRYPT_MODE, secretkey);
         byte[] encrypted = cipher.doFinal(Message.getBytes());
+        String MessageHex = HexConvert(encrypted);
         System.out.println("Your encoded message is: " + Message);
+                           
      //Decryption
         cipher.init(Cipher.DECRYPT_MODE,secretkey);
-        byte[] decrypted = cipher.doFinal(Message.getBytes();
-        System.out.println("Your message in plaintext is: " + Message);
+        byte[] decrypted = cipher.doFinal(ByteConvert(MessageHex));
+        String MessageDecrypt = HexConvert(decrypted)
+        System.out.println("Your message in plaintext is: " + Decrypted);
                                           
 }
         
