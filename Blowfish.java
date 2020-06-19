66d864dd2dd5ab5fba6612c16661ea389a85665c
Hammad Khan <59400114+ha36ad@users.noreply.github.com>
2020-06-19 17:09:08 -0400

    Update Blowfish.java

--- Blowfish.java
+++ Blowfish.java
@@ -2,28 +2,33 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
 import java.util.Scanner;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 
-public static String blowfish (String password); {
-     //Ask user for a message + key
-    public static void main(String[] args) {
-    InputKey = new Scanner(System.in);
-    System.out.println("Enter your key");
-    String KeyInput = KeyInput.nextLine()
-    String key;
-    InputMessage = new Scanner(System.in);
-    System.out.println("Enter message");
-    String MessageInput = MessageInput.nextLine()
-    String message;
-        
-    }
-    byte [] KeyData = (key).getBytes();
-    SecretKeySpec SK = new SecretKeySpec (MessageData, "Blowfish");
-    Cipher cipher = Cipher.getInstance ("Blowfish");
-    cipher.int(Cipher.ENCRYPT_MODE, securekey);
-    
-   
-    //Encrypting the message
-    byte [] encrypted = cipher.doFinal (message.getBytes());
-        
-        
+public class Blowfish {
+
+    public static void main(String[] args) throws Exception {
+       //Asks user for a message
+        Scanner InputMessage = new Scanner(System.in);
+        System.out.println("Enter message");
+        String Message = InputMessage.nextLine();  // Read user input
+        System.out.println("Your message in plaintext is: " + Message);
+
+ //Key generation
+        KeyGenerator kgen = KeyGenerator.getInstance("Blowfish");
+        SecretKey skey = kgen.generateKey();
+        byte[] skeyb = skey.getEncoded();
+        SecretKeySpec secretkey = new SecretKeySpec(skeyb, "Blowfish");
+
+      //Encryption
+        Cipher cipher = Cipher.getInstance("Blowfish");
+        cipher.init(Cipher.ENCRYPT_MODE, secretkey);
+        byte[] encrypted = cipher.doFinal(Message.getBytes());
+        System.out.println("Your encoded message is: " + Message);
+     //Decryption
+        cipher.init(Cipher.DECRYPT_MODE,secretkey);
+        byte[] decrypted = cipher.doFinal(Message.getBytes();
+        System.out.println("Your message in plaintext is: " + Message);
+                                          
 }
+        
