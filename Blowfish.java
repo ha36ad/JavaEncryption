d9b42ed1e9f28642c040e09f7cc013ca5e6d6f60
Hammad Khan <59400114+ha36ad@users.noreply.github.com>
2020-06-19 21:13:52 -0400

    Update Blowfish.java

--- Blowfish.java
+++ Blowfish.java
@@ -4,7 +4,6 @@ import java.util.Base64;
 import java.util.Scanner;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-
 public class Blowfish {
 
     public static void main(String[] args) throws Exception {
@@ -14,7 +13,7 @@ public class Blowfish {
         String Message = InputMessage.nextLine();  
         System.out.println("Your message in plaintext is: " + Message);
        
- //Key generation
+      //Key generation
         KeyGenerator kgen = KeyGenerator.getInstance("Blowfish");
         SecretKey skey = kgen.generateKey();
         byte[] skeyb = skey.getEncoded();
@@ -24,14 +23,18 @@ public class Blowfish {
         Cipher cipher = Cipher.getInstance("Blowfish");
         cipher.init(Cipher.ENCRYPT_MODE, secretkey);
         byte[] encrypted = cipher.doFinal(Message.getBytes());
-        String MessageHex = HexConvert(encrypted);
-        System.out.println("Your encoded message is: " + Message);
+        Base64.Encoder encoder = Base64.getEncoder();
+        encrypted_msg = new String(encoder.encode(encrypted));
+        System.out.println("Your encoded message is: " + encrypted_msg);
                            
      //Decryption
+        cipher = Cipher.getInstance("Blowfish");
         cipher.init(Cipher.DECRYPT_MODE,secretkey);
-        byte[] decrypted = cipher.doFinal(ByteConvert(MessageHex));
-        String MessageDecrypt = HexConvert(decrypted)
-        System.out.println("Your message in plaintext is: " + Decrypted);
+        byte[] decrypted = cipher.doFinal(encrypted_msg.getBytes());
+        Base64.Decoder decoder = Base64.getDecoder();
+        decrypted_String = new String(decoder.decode(decrypted));        
+        System.out.println("Your message in plaintext is: " + decrypted);
                                           
+}
 }
         
